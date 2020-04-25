040a66050e025e33831cdb0a3da5dd3dfa07004c
xuchang970621 <cxu30@hawk.iit.edu>
2020-04-24 19:56:18 -0500

    lot id database updated.

--- cs487/parking/admin.py
+++ cs487/parking/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import ParkingLots
-from .models import ParkingSpots
+from .models import ParkingLot
+from .models import ParkingSpot
 from .models import Address
 from .models import Payment
 
-admin.site.register(ParkingLots)
-admin.site.register(ParkingSpots)
+admin.site.register(ParkingLot)
+admin.site.register(ParkingSpot)
 admin.site.register(Address)
 admin.site.register(Payment)
--- cs487/parking/models.py
+++ cs487/parking/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class ParkingLots(models.Model):
+class ParkingLot(models.Model):
     lot_id = models.CharField(primary_key=True, max_length=6)
     lot_name = models.CharField(max_length=200)
     lot_hours = models.CharField(max_length=200)
 
-class ParkingSpots(models.Model):
-    lot_id = models.CharField(primary_key=True, max_length=6)
+class ParkingSpot(models.Model):
+    spot_id = models.CharField(primary_key=True, max_length=6)
+    lot_id = models.ForeignKey(ParkingLot, on_delete=models.CASCADE)
+    spot_number = models.CharField(max_length=6)
     spot_occupancy = models.CharField(max_length=1)
     spot_availabiliy = models.CharField(max_length=200)
 
@@ -21,8 +23,8 @@ class Address(models.Model):
     postcode = models.CharField(max_length=6)
 
 class Payment(models.Model):
-    address = models.ForeignKey(Address, on_delete=models.CASCADE)
-    credit_card = models.CharField(max_length=16)
+    credit_card = models.CharField(primary_key=True, max_length=16)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE) 
     expiration_month = models.CharField(max_length=2)
     expiration_year = models.CharField(max_length=4)
     cvv = models.CharField(max_length=3)
